7dd25ce8817f993cf4da364f0ce9b5701adfc5a7
ToHellForever <dim.anosoff2018@yandex.ru>
2026-05-09 20:22:29 +0300

    логика отправки отчётов по расписанию (под вопросом)

--- partner_app/tasks.py
+++ partner_app/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 from django.core.mail import EmailMessage
 from django.utils import timezone
+from django.core.files.base import ContentFile
 from datetime import datetime, timedelta
 from .models import ReportSchedule, SalesReport
 from .utils import generate_sales_report
