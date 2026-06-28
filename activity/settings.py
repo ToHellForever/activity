02746280d64c1e7593fdfe66fd5613e26d0224c8
ToHellForever <dim.anosoff2018@yandex.ru>
2026-06-28 08:58:28 +0300

    проверка actions

--- activity/settings.py
+++ activity/settings.py
@@ -6,7 +6,7 @@ from pathlib import Path
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# yjdj
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
