551b3f4b7cef27517dc04f8538b496539a627687
ToHellForever <dim.anosoff2018@yandex.ru>
2026-06-30 10:42:18 +0300

    .

--- payment/urls.py
+++ payment/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("handle_package_change_choice/", views.handle_package_change_choice, name="handle_package_change_choice"),
     path("create_invoice/<int:package_id>/", views.create_invoice, name="create_invoice"),
     path("webhook/", views.yookassa_webhook, name="yookassa_webhook"),
+    path("package_success/<int:package_id>/", views.package_success, name="package_success"),
     path("success/<int:order_id>/", views.payment_success, name="payment_success"),
     path('refund/<int:order_id>/', views.refund_ticket, name='refund_ticket'),
     path("pay-reserved/<int:order_id>/", views.pay_reserved_order, name="pay_reserved_order"),
